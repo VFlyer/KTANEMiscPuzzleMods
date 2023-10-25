86134b2bf5ce7882a5d14104864473c3a148f1fa
Vincent Flyer (Vincent Chan) <35939457+VFlyer@users.noreply.github.com>
2023-10-24 22:31:04 -0700

    Prep Work on Encrypted Sudoku...
    This will be a nasty one...

---
+++ Assets/Modules/Encrypted Sudoku/ESudokuTile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESudokuTile : MonoBehaviour {
+
+
+    protected bool hPanelRequired = false;
+    protected bool vPanelRequired = false;
+
+    public bool requireHorizontalPanel { get { return hPanelRequired; } }
+    public bool requireVerticalPanel { get { return vPanelRequired; } }
+
+    [SerializeField]
+    protected GameObject hPanelContents, vPanelContents;
+    protected Renderer centerTileRenderer;
+
+    EncryptedSudokuScript baseScript;
+	public void AssignCore(EncryptedSudokuScript mainHandler)
+    {
+        baseScript = mainHandler;
+        SetupCell();
+    }
+    protected virtual void SetupCell()
+    {
+
+    }
+    public virtual string GetTPCellHelpInfo()
+    {
+        return "";
+    }
+}
---
+++ Assets/Modules/Encrypted Sudoku/EncryptedSudokuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncryptedSudokuScript : MonoBehaviour {
+
+	public KMBombModule modSelf;
+	public KMBombInfo bombInfo;
+	public ESudokuTile[] listedTiles;
+	public KMSelectable[] directionSelectables;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
